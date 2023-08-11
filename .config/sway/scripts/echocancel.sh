f8cb7b43f0cda461c3056cd93ad404213c680cf2
noel curray <noel.curray@gmail.com>
2023-08-11 09:22:35 +0800

    backup: invalid partition table!

---
+++ .config/sway/scripts/echocancel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+pactl unload-module module-echo-cancel
+pactl load-module module-echo-cancel aec_method=webrtc source_name=echocancel sink_name=echocancel1
+pactl set-default-source echocancel
+pactl set-default-sink echocancel1
+
+
--- .config/sway/scripts/status_bar.sh
+++ .config/sway/scripts/status_bar.sh
@@ -4,9 +4,8 @@
 notify_plug=false
 notify_bat_full=false
 brightness_100=false
-
+battery_icon=none
 function brightnesstoggle() {
-	#read -p "$brightness_status" < /dev/tty
 	if [ brightness_100 = false ] ; then
 		brightnessctl set 100%
 		brightness_100=true
@@ -15,16 +14,52 @@ function brightnesstoggle() {
 	fi
 }
 
+function batt_icon_charging() {
+	case $1 in
+		100) battery_icon="󰂅" ;;
+		9[0-9]) battery_icon="󰂋" ;;
+		8[0-9]) battery_icon="󰂊" ;;
+		7[0-9]) battery_icon="󰢞" ;;
+		6[0-9]) battery_icon="󰂉" ;;
+		5[0-9]) battery_icon="󰢝" ;;
+		4[0-9]) battery_icon="󰂈" ;;
+		3[0-9]) battery_icon="󰂇" ;;
+		2[0-9]) battery_icon="󰂆" ;;
+		1[0-9]) battery_icon="󰢜" ;;
+		0[0-9]) battery_icon="󰢟" ;;
+	esac
+
+}
+
+
+function batt_icon_discharging() {
+	case $1 in
+		100) battery_icon="󰁹" ;;
+		9[0-9]) battery_icon="󰂂" ;;
+		8[0-9]) battery_icon="󰂁" ;;
+		7[0-9]) battery_icon="󰂀" ;;
+		6[0-9]) battery_icon="󰁿" ;;
+		5[0-9]) battery_icon="󰁾" ;;
+		4[0-9]) battery_icon="󰁽" ;;
+		3[0-9]) battery_icon="󰁼" ;;
+		2[0-9]) battery_icon="󰁻" ;;
+		1[0-9]) battery_icon="󰁺" ;;
+		0[0-9]) battery_icon="󰂎" ;;
+	esac
+
+}
+
 function battery_checker() {
 	date_display=$(date +'%Y-%m-%d %T')
 	batt_stat=$(cat /sys/class/power_supply/BAT0/status)
 	batt_cap=$(cat /sys/class/power_supply/BAT0/capacity)
 	sys_temp=$(acpi -t | cut -d " " -f 4)
 
-      if [ $batt_stat = "Charging" ] ; then 
+      if [ $batt_stat = "Charging" ] ; then
+	      batt_icon_charging $batt_cap
       	if [ $batt_cap -ge 100 ] ; then
       		if [ $notify_bat_full = false ] ; then
-      			dunstify -h string:bgcolor:"#007500" "Battery Full." && paplay ~/Downloads/battery_full.mp3
+      			dunstify -i "󰂃" -u critical -h string:bgcolor:"#007500" -h string:frcolor:"#007500" "Battery Full." && paplay --volume=98304 ~/Downloads/battery_full.mp3
       			notify_bat_full=true
       		fi 
       	else
@@ -34,17 +69,18 @@ function battery_checker() {
 	if [ $batt_cap -ge 21 ] ; then notify_plug=false ; fi
 
       else ## discharging
+	      batt_icon_discharging $batt_cap
       	notify_bat_full=false
       	if [ $batt_cap -le 20 ] ; then
       		if [ $notify_plug = false ] ; then
-      			dunstify "Please plug the charger." -u critical && paplay ~/Downloads/please_charge.mp3
+      			dunstify "Please plug the charger." -u critical && paplay --volume=98304 ~/Downloads/please_charge.mp3
       			notify_plug=true
       		fi
       	fi
       	
       fi
 
-	echo $sys_temp°C [$batt_stat \($batt_cap\%\)] $date_display 
+	echo 󰔏 $sys_temp°C \| $battery_icon\($batt_cap\%\) \| $date_display 
 }
 
 while true 
