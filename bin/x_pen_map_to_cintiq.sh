8b9d6fe51dfa2ed75141c194cd21765da6927c9a
noel curray <noel.curray@gmail.com>
2023-08-16 16:23:00 +0800

    2023-08-16

--- bin/x_pen_map_to_cintiq.sh
+++ bin/x_pen_map_to_cintiq.sh
@@ -4,10 +4,8 @@ GETSTYLUSID=`xsetwacom list devices`
 
 ID_STYLUS=$(echo $GETSTYLUSID | cut -d " " -f 7)
 ID_ERASER=$(echo $GETSTYLUSID | cut -d " " -f 16)
-SCREEN_TARGET=$(echo `xrandr| grep HDMI | cut -d " " -f 3`)
-notify-send "stylus id is: $ID_STYLUS"
-notify-send "eraser id is: $ID_ERASER"
-notify-send "screen target is: $SCREEN_TARGET"
-xsetwacom set $ID_STYLUS MapToOutput $SCREEN_TARGET
-xsetwacom set $ID_ERASER  MapToOutput $SCREEN_TARGET
+#SCREEN_TARGET=$(echo $(xrandr| grep HDMI | cut -d " " -f 3))
+xsetwacom set $ID_STYLUS MapToOutput "HDMI-1"
+xsetwacom set $ID_ERASER  MapToOutput "HDMI-1"
+dunstify "Wacom pen is now mapped in HDMI-1"
 
