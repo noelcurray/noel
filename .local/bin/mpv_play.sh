9a896be6759f2dcfb184cebfa144ba0732999060
noel curray <noel.curray@gmail.com>
2023-08-23 12:08:46 +0800

    Wed Aug 23 12:08:46 PM PST 2023

--- .local/bin/mpv_play.sh
+++ .local/bin/mpv_play.sh
@@ -5,6 +5,6 @@ else
 	clipboardType=$(xclip -sel clip -o)
 fi
 
-mpv $clipboardType & dunstify "MPV" "playing: $clipboardType" -h string:bgcolor:"#4B1F52" -i "/hdd/noel/icons/mpv-logo-32.png"
+mpv $clipboardType & dunstify "MPV" "playing: $clipboardType" -h string:bgcolor:"#8A5B92" -i "/hdd/noel/icons/mpv-logo-32.png"
 
 
